6b4da6196a685c325759982c3cfcbc18499b761e
“shmenkare” <shmenkare@gmail.com>
2023-11-29 00:52:06 -0400

    Update Curso sql yt.sql

--- Curso sql yt.sql
+++ Curso sql yt.sql
@@ -104,3 +104,37 @@ SELECT DISTINCT Calificacion FROM Calificaciones;
 SELECT * FROM Calificaciones WHERE Nombre = 'Daniela' AND Calificacion < 9;
 SELECT * FROM Calificaciones WHERE Nombre = 'Daniela' OR Calificacion > 9;
 SELECT * FROM Calificaciones WHERE NOT Nombre = 'Daniela';
+
+--COMANDO BETWEEN
+
+SELECT * FROM Calificaciones; 
+
+SELECT * FROM Calificaciones WHERE Calificacion BETWEEN 6 AND 8 AND NOT Nombre = 'Pedro';
+
+--COMNANDO ORDE BY
+
+SELECT * FROM Calificaciones ORDER BY Calificacion ASC; 
+SELECT * FROM Calificaciones ORDER BY Calificacion DESC; 
+
+-- ALIAS
+
+SELECT idAlumno AL, Nombre NM, Calificacion CAL FROM Calificaciones CAL WHERE CAL.Nombre = 'RUBEN';
+
+-- TRUNCATE
+
+INSERT INTO empleado VALUES(1,'Daniel','Director');
+INSERT INTO empleado VALUES(2,'Uriel','SubDirector');
+INSERT INTO empleado VALUES(3,'Martin','Gerente');
+INSERT INTO empleado VALUES(3,'Rosa','Gerente');
+INSERT INTO empleado VALUES(3,'Martha','Gerente');
+
+SELECT * FROM empleado;
+
+TRUNCATE TABLE empleado;
+
+--DROP
+DROP TABLE empleado;
+
+USE master;
+
+DROP DATABASE PracticaYT;
