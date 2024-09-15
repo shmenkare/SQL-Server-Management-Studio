b45b3ae400b55456399d2d7ceca874cb72c85161
“shmenkare” <shmenkare@gmail.com>
2024-09-14 22:29:00 -0400

    practica

--- Temas querys.sql
+++
@@ -1,9 +0,0 @@
-use MusicListDB;
-
-select * from temas where Genero = 2;
-
-update temas set Proyecto = 1 where Proyecto = 2;
-
-select Nombre, NomProyecto from Temas JOIN Proyecto ON Nombre = 'Sanitarios';
-
-SELECT Nombre, NomProyecto FROM Temas JOIN Proyecto ON Id_proyecto = 2;
