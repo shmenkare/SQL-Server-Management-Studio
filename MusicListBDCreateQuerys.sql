70c4ca7024e9eda0adbfb556e9cc24ff58187730
“shmenkare” <shmenkare@gmail.com>
2024-09-18 17:45:24 -0400

    Update MusicListBDCreateQuerys.sql

--- MusicListBDCreateQuerys.sql
+++ MusicListBDCreateQuerys.sql
@@ -2,7 +2,6 @@
 go
 
 create database MusicListBD;
-go 
 
 use MusicListBD;
 
@@ -53,7 +52,7 @@ go
 create table Locales
 (
     Id_local int identity(1,1) not null,
-    Local varchar(25),
+    Nombre varchar(25),
     constraint PK_Locales primary key (Id_local)
 );
 go
@@ -69,17 +68,16 @@ go
 create table Proyecto
 (
     Id_proyecto int identity(1,1) not null,
-    Proyecto varchar(25),
+    Nombre varchar(25),
     constraint PK_Proyecto primary key (Id_proyecto)
 );
 go
 
 create table TemasLocales
 (
-    Id_TemasLocales int identity(1,1) not null,
     Id_local int not null,
     Id_tema int not null,
-    constraint PK_TemasLocales primary key (Id_TemasLocales),
+    constraint PK_TemasLocales primary key (Id_local, Id_tema),
     constraint Permitidos_en foreign key (Id_local) references Locales(Id_local),
     constraint Para_locales foreign key (Id_tema) references Temas(Id_tema)
 );
@@ -87,10 +85,9 @@ go
 
 create table TemasPorBanda
 (
-    Id_TemasPorBanda int identity(1,1) not null,
     Id_Conf int not null,
     Id_tema int not null,
-    constraint PK_TemasPorBanda primary key (Id_TemasPorBanda),
+    constraint PK_TemasPorBanda primary key (Id_Conf,Id_tema),
     constraint Tocados_por_banda foreign key (Id_Conf) references Config_Banda(Id_Conf),
     constraint Temas_que_toca foreign key (Id_tema) references Temas(Id_tema)
 );
@@ -98,17 +95,59 @@ go
 
 create table TemasProyecto
 (
-    Id_TemasProyecto int identity(1,1) not null,
     Id_proyecto int not null,
     Id_tema int not null,
-    constraint PK_TemasProyecto primary key (Id_TemasProyecto),
+    constraint PK_TemasProyecto primary key (Id_proyecto,Id_tema),
     constraint Tocados_por foreign key (Id_proyecto) references Proyecto(Id_proyecto),
     constraint Para_proyecto foreign key (Id_tema) references Temas(Id_tema)
 );
-go
 
 --drop database MusicListBD;
+
+insert into MusicListBD.dbo.Banda_interprete(Nombre)
+select Nombre
+from MusicListDB.dbo.Banda_interprete
+
+insert into MusicListBD.dbo.Generos(Genero)
+select Genero
+from MusicListDB.dbo.Generos
+
+insert into MusicListBD.dbo.Idiomas(Idioma)
+select Idioma
+from MusicListDB.dbo.Idiomas
 
 insert into MusicListBD.dbo.temas(Nombre,Id_Banda_interprete,Id_genero,Id_idioma,Año,Tonalidad,Programa_GT,Programa_VE,Tema_montado,Duracion)
 select Nombre,Id_Banda_interprete,Id_genero,Id_idioma,Año,Tonalidad,Programa_GT,Programa_VE,Tema_montado,Duracion
 from MusicListDB.dbo.Temas;
+
+insert into MusicListBD.dbo.Config_Banda(Configuracion)
+select Configuracion
+from MusicListDB.dbo.Config_Banda
+
+insert into MusicListBD.dbo.Locales(Nombre)
+select Nombre
+from MusicListDB.dbo.Locales
+
+insert into MusicListBD.dbo.Proyecto(Nombre)
+select Nombre
+from MusicListDB.dbo.Proyecto
+
+insert into MusicListBD.dbo.TemasLocales(Id_local,Id_tema)
+select Id_local,Id_tema
+from MusicListDB.dbo.TemasLocales
+
+insert into MusicListBD.dbo.TemasPorBanda(Id_Conf,Id_tema)
+select Id_Conf,Id_tema
+from MusicListDB.dbo.TemasPorBanda
+
+insert into MusicListBD.dbo.TemasProyecto(Id_proyecto,Id_tema)
+select Id_proyecto,Id_tema
+from MusicListDB.dbo.TemasProyecto
+
+
+
+
+
+
+
+
